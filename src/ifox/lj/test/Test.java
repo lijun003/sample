25d23b8806996e0a4b053417300bb77fbe1fca0a
lijun003 <lijun359714823@163.com>
2016-06-27 19:13:14 +0800

    haaah

--- src/ifox/lj/test/Test.java
+++ src/ifox/lj/test/Test.java
@@ -1,10 +1,8 @@
 package ifox.lj.test;
 
-/**
- * Created by lijun on 16-6-26.
- */
 public class Test {
     public static void main(String args[]){
         System.out.println("hello world!");
+
     }
 }
